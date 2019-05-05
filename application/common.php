710bec906172983fc72e147b9b0f7e671cd25ecc
XiaoDi <31346973+xiaodit@users.noreply.github.com>
2019-05-05 13:33:35 +0800

    Update common.php

--- application/common.php
+++ application/common.php
@@ -10,41 +10,6 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
-use think\auth\Auth;
-use app\admin\model\rbac\RuleModel;
-use app\lib\Category;
-
-function checkMenuAuth($path)
-{
-    // 获取auth实例
-    $auth = Auth::instance();
-
-    // 检测权限
-    if ($auth->check($path, session('admin_user'))) {
-        return true;
-    }
-
-    return false;
-}
-
-/**
- *
- */
-function showParentMenu($pid)
-{
-    $rule = new RuleModel;
-    $data = $rule->order('pid asc')->select();
-    $category = new Category(array('id','pid','title','cname'));
-    $level2 = $category->getChild($pid, $data->toArray());
-    foreach ($level2 as $v) {
-        if (checkMenuAuth($v['name'])) {
-            return true;
-            continue;
-        }
-    }
-
-    return false;
-}
 
 /**
  * 路由调整
