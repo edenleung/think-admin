51bac14c836eca4faa1fafd0ac267deed74fcddb
XiaoDi <31346973+xiaodit@users.noreply.github.com>
2019-05-05 14:27:39 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'mysql56',
+    'hostname'        => 'mysql',
     // 数据库名
     'database'        => 'think',
     // 用户名
