1cb0d51bb5daf954bb63d3f7106f9412a513b0d0
xiaodi <liangjinbiao@live.com>
2019-12-14 19:19:41 +0800

    添加 随机生成指定长度字符串

--- app/common.php
+++ app/common.php
@@ -10,3 +10,21 @@ declare(strict_types=1);
  * @copyright 2019 Xiaodi
  * @license  https://github.com/xiaodit/think-admin/blob/6.0/LICENSE.txt
  */
+
+/**
+ * 随机生成指定长度字符串
+ *
+ * @param integer $len
+ * @return void
+ */
+function randomKey($len = 11)
+{
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~0123456789#$%^&';
+    $pass = [];
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < 10; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass);
+}
