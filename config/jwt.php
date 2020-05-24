31e0dc508c0faeb143291db3a7462ec7b20620e7
Eden Leung <31346973+edenleung@users.noreply.github.com>
2020-05-24 11:22:32 +0800

    适配最新版 jwt

--- config/jwt.php
+++ config/jwt.php
@@ -11,25 +11,30 @@
  */
 
 return [
-    'default' => [
-        'uniqidKey'        => 'uid',
-        'signerKey'        => 'RvAjzUhtJs',
-        'notBefore'        => 0,
-        'expiresAt'        => 3600,
-        'refreshTTL'       => 7200,
-        'signer'           => 'Lcobucci\JWT\Signer\Hmac\Sha256',
-        'type'             => 'Header',
-        'refresh'          => 50001,
-        'relogin'          => 50002,
-        'iss'              => '',
-        'aud'              => '',
-        'automaticRenewal' => true,
-    ],
-    'user' => [
-        'inject' => true,
-        'model'  => 'app\common\model\User',
-    ],
-    'blacklist' => [
-        'cacheName' => 'blacklist',
-    ],
+    'default' => 'admin',
+    'apps' => [
+        'admin' => [
+            'token' => [
+                'uniqidKey'        => 'uid',
+                'signerKey'        => 'RvAjzUhtJs',
+                'notBefore'        => 0,
+                'expiresAt'        => 3600,
+                'refreshTTL'       => 7200,
+                'signer'           => 'Lcobucci\JWT\Signer\Hmac\Sha256',
+                'type'             => 'Header',
+                'refresh'          => 50001,
+                'relogin'          => 50002,
+                'iss'              => '',
+                'aud'              => '',
+                'automaticRenewal' => true,
+            ],
+            'user' => [
+                'bind' => true,
+                'model'  => 'app\common\model\User',
+            ],
+            'blacklist' => [
+                'cacheKey' => 'blacklist',
+            ],
+        ]
+    ]
 ];
