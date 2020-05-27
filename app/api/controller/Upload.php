ca94bf1f3079047ab96e894379f1ead633b249f5
Eden Leung <liangjinbiao@live.com>
2020-05-27 15:31:54 +0000

    Apply fixes from StyleCI

--- app/api/controller/Upload.php
+++ app/api/controller/Upload.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of TAnt.
+ * @link     https://github.com/edenleung/think-admin
+ * @document https://www.kancloud.cn/manual/thinkphp6_0
+ * @contact  QQ Group 996887666
+ * @author   Eden Leung 758861884@qq.com
+ * @copyright 2019 Eden Leung
+ * @license  https://github.com/edenleung/think-admin/blob/6.0/LICENSE.txt
+ */
+
 namespace app\api\controller;
 
 use app\BaseController;
@@ -15,7 +25,7 @@ class Upload extends BaseController
     public function file()
     {
         $file = $this->request->file('file');
-        
+
         $savename = \think\facade\Filesystem::disk('public')->putFile('topic', $file);
 
         return $this->sendSuccess([
