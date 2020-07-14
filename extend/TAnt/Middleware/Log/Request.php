0d1bb54795d681a9aa81235652c7181af3a26794
梁锦标 <liangjinbiao@live.com>
2020-07-14 13:03:29 +0800

    添加 请求日志中间件

---
+++ extend/TAnt/Middleware/Log/Request.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TAnt.
+ * @link     https://github.com/edenleung/think-admin
+ * @document https://www.kancloud.cn/manual/thinkphp6_0
+ * @contact  QQ Group 996887666
+ * @author   Eden Leung 758861884@qq.com
+ * @copyright 2019 Eden Leung
+ * @license  https://github.com/edenleung/think-admin/blob/6.0/LICENSE.txt
+ */
+
+namespace app\api\middleware;
+
+class Request
+{
+    public function handle($request, \Closure $next)
+    {
+        \think\facade\Log::record($request->url() . '接口请求:' . $request->method(), 'info');
+        \think\facade\Log::record($request->param(), 'info');
+
+        return $next($request);
+    }
+}
---
+++ extend/TAnt/Middleware/Log/Response.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of TAnt.
+ * @link     https://github.com/edenleung/think-admin
+ * @document https://www.kancloud.cn/manual/thinkphp6_0
+ * @contact  QQ Group 996887666
+ * @author   Eden Leung 758861884@qq.com
+ * @copyright 2019 Eden Leung
+ * @license  https://github.com/edenleung/think-admin/blob/6.0/LICENSE.txt
+ */
+
+namespace app\api\middleware;
+
+class Response
+{
+    public function handle($request, \Closure $next)
+    {
+        $response = $next($request);
+
+        \think\facade\Log::record($request->url() . '接口返回' . $request->method(), 'info');
+        \think\facade\Log::record($response->getData(), 'info');
+
+        return $response;
+    }
+}
