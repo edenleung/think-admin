1728bb7233c07a6174f708d5f494102972b2c5f4
梁锦标 <liangjinbiao@live.com>
2020-11-04 17:25:33 +0800

    jwt 配置

--- config/jwt.php
+++ config/jwt.php
@@ -11,48 +11,31 @@
  */
 
 return [
-    'default' => 'admin',
-    'apps'    => [
+    'stores' => [
         'admin' => [
+            'sso' => [
+                'enable' => false,
+            ],
             'token' => [
-                'uniqidKey'        => 'uid',
-                'signerKey'        => 'RvAjzUhtJs',
-                'notBefore'        => 0,
-                'expiresAt'        => 3600,
-                'refreshTTL'       => 7200,
-                'signer'           => 'Lcobucci\JWT\Signer\Hmac\Sha256',
-                'type'             => 'Header',
-                'refresh'          => 50001,
-                'relogin'          => 50002,
-                'iss'              => 'client.tant',
-                'aud'              => 'server.tant',
-                'automaticRenewal' => false,
+                'unique_id_key'    => 'uid',
+                'signer_key'    => 'tant',
+                'not_before'    => 0,
+                'expires_at'    => 3600,
+                'refresh_ttL'   => 7200,
+                'signer'       => 'Lcobucci\JWT\Signer\Hmac\Sha256',
+                'type'         => 'Header',
+                'relogin_code'      => 50001,
+                'refresh_code'      => 50002,
+                'iss'          => 'client.tant',
+                'aud'          => 'server.tant',
+                'automatic_renewal' => false,
             ],
             'user' => [
-                'bind'   => true,
-                'model'  => 'app\\common\\model\\User',
-            ],
-        ],
-        'wechat' => [
-            'token' => [
-                'uniqidKey'        => 'uid',
-                'signerKey'        => 'AYIB8IVcwSE',
-                'notBefore'        => 0,
-                'expiresAt'        => 36000,
-                'refreshTTL'       => 72000,
-                'signer'           => 'Lcobucci\JWT\Signer\Hmac\Sha256',
-                'type'             => 'Header',
-                'refresh'          => 50001,
-                'relogin'          => 50002,
-                'iss'              => 'client.tant',
-                'aud'              => 'server.tant',
-                'automaticRenewal' => false,
-            ],
-            'user' => [
-                'bind'   => true,
-                'model'  => 'app\\common\\model\\Member',
-            ],
-        ],
+                'bind' => true,
+                'class'  => 'app\\common\\model\\User',
+
+            ]
+        ]
     ],
     'manager' => [
         // 缓存前缀
@@ -60,6 +43,6 @@ return [
         // 黑名单缓存名
         'blacklist' => 'blacklist',
         // 白名单缓存名
-        'whitelist' => 'whitelist',
-    ],
+        'whitelist' => 'whitelist'
+    ]
 ];
