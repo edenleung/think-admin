5b0c3115f78ab5e65376208de2a4191423ebf700
Eden Leung <edenleung@users.noreply.github.com>
2020-12-05 09:43:29 +0000

    Apply fixes from StyleCI

--- config/middleware.php
+++ config/middleware.php
@@ -1,5 +1,15 @@
 <?php
-// 中间件配置
+
+/*
+ * This file is part of TAnt.
+ * @link     https://github.com/edenleung/think-admin
+ * @document https://www.kancloud.cn/manual/thinkphp6_0
+ * @contact  QQ Group 996887666
+ * @author   Eden Leung 758861884@qq.com
+ * @copyright 2019 Eden Leung
+ * @license  https://github.com/edenleung/think-admin/blob/6.0/LICENSE.txt
+ */
+
 return [
     // 别名或分组
     'alias'    => [],
