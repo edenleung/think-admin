d4a7ddb1ece653c0f5f6187c13f5553f9a95289c
Eden Leung <31346973+edenleung@users.noreply.github.com>
2021-03-18 13:56:16 +0800

    Update and rename 2020-08-15.sql to member.sql

--- database/member.sql
+++ database/member.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `member` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
   `nickname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
